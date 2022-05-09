fef5355cb08d0a22f444de552d0b4c48a6ed1414
Erin Winking <erin@douglasavenue.com>
2022-05-09 12:52:08 -0500

    Route protection middleware set up. Added profile route.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -70,6 +70,19 @@ const loginUser = asyncHandler(async (req, res) => {
 	}
 })
 
+// @desc Get Current User!
+// @route /api/users/me
+// @access Protected
+
+const getMe = asyncHandler(async (req, res) => {
+	const user = {
+		id: req.user._id,
+		email: req.user.email,
+		name: req.user.name,
+	}
+	res.status(200).json(user)
+})
+
 // Generate JSON Web Token
 const generateToken = (id) => {
 	return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -80,4 +93,5 @@ const generateToken = (id) => {
 module.exports = {
 	registerUser,
 	loginUser,
+	getMe,
 }
