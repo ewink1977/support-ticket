df6fa011fed624a02a79b1e19a330c53285e7116
Erin Winking <erin@douglasavenue.com>
2022-05-10 16:36:20 -0500

    Set up ticket UI in the front end, added the ability to close tickets.

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -15,8 +15,54 @@ const createTicket = async (ticketData, token) => {
 	return response.data
 }
 
+// Get All User Tickets
+const getTickets = async (token) => {
+	const config = {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	}
+
+	const response = await axios.get(API_URL, config)
+
+	return response.data
+}
+
+// Get All User Tickets
+const getTicket = async (ticketId, token) => {
+	const config = {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	}
+
+	const response = await axios.get(API_URL + ticketId, config)
+
+	return response.data
+}
+
+// Get All User Tickets
+const closeTicket = async (ticketId, token) => {
+	const config = {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	}
+
+	const response = await axios.put(
+		API_URL + ticketId,
+		{ status: 'closed' },
+		config
+	)
+
+	return response.data
+}
+
 const ticketService = {
 	createTicket,
+	getTickets,
+	getTicket,
+	closeTicket,
 }
 
 export default ticketService
