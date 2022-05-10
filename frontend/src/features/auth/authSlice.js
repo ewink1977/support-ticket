164cb8a5cf5b22cca750db0b7dfd6d0951e05735
Erin Winking <erin@douglasavenue.com>
2022-05-09 19:46:06 -0500

    Errors fixed. Register and Logout working as intended.

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,8 +1,10 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import authService from './authService'
 
+const user = JSON.parse(localStorage.getItem('user'))
+
 const initialState = {
-	user: null,
+	user: user ? user : null,
 	isError: false,
 	isSuccess: false,
 	isLoading: false,
@@ -33,6 +35,11 @@ export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
 	console.log(user)
 })
 
+// Logout User
+export const logout = createAsyncThunk('auth/logout', async () => {
+	await authService.logout()
+})
+
 export const authSlice = createSlice({
 	name: 'auth',
 	initialState,
@@ -58,6 +65,9 @@ export const authSlice = createSlice({
 				state.isLoading = false
 				state.isError = true
 				state.message = action.payload
+				state.user = null
+			})
+			.addCase(logout.fulfilled, (state) => {
 				state.user = null
 			})
 	},
