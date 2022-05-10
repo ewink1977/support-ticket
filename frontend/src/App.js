6e914c6f4eacd41cf6251fff470bc7372a8bfc13
Erin Winking <erin@douglasavenue.com>
2022-05-09 19:23:53 -0500

    Up to lesson 132, currently working on an error issue.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Header from './components/Header'
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,4 +1,7 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { register, reset } from '../features/auth/authSlice'
 import { FaUser } from 'react-icons/fa'
 import { toast } from 'react-toastify'
 
@@ -12,6 +15,25 @@ function Register() {
 
 	const { name, email, password, password2 } = formData
 
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
+
+	const { user, isLoading, isSuccess, isError, message } = useSelector(
+		(state) => state.auth
+	)
+
+	useEffect(() => {
+		if (isError) {
+			toast.error(message)
+		}
+
+		if (isSuccess || user) {
+			navigate('/')
+		}
+
+		dispatch(reset())
+	}, [isError, isSuccess, user, message, navigate, dispatch])
+
 	const onChange = (e) => {
 		setFormData((prevState) => ({
 			...prevState,
@@ -24,6 +46,14 @@ function Register() {
 
 		if (password !== password2) {
 			toast.error('Passwords do not match.')
+		} else {
+			const userData = {
+				name,
+				email,
+				password,
+			}
+
+			dispatch(register(userData))
 		}
 	}
 
